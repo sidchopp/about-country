cd7ad71cb75164f7aba595fdd3953f77dc1ed726
Sid <sidchopp@gmail.com>
2021-08-16 13:37:58 -0600

    neighbouring country appeared

--- script.js
+++ script.js
@@ -50,19 +50,16 @@ const renderCountry = function (data, className = '') {
     <div class="country__data">
       <h3 class="country__name">${data.name}</h3>
       <h4 class="country__region">${data.region}</h4>
-      <p class="country__row"><span>👫</span>${(
-      +data.population / 1000000
-    ).toFixed(1)} people</p>
+      <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} people</p>
       <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
       <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
     </div>
-  </article>
-  `;
+  </article>`;
   countriesContainer.insertAdjacentHTML('beforeend', html);
   countriesContainer.style.opacity = 1;
 };
 
-// with function declaration 
+// with function expression
 // const getCountryData = function (country) {
 //   fetch(`https://restcountries.eu/rest/v2/name/${country}?fullText=true`)
 //     .then(function (response) {
@@ -83,7 +80,7 @@ const getCountryData = function (country) {
   fetch(`https://restcountries.eu/rest/v2/name/${country}?fullText=true`)
     .then(response => response.json())
     .then(data => {
-      renderCountry(data[0])
+      renderCountry(data[0]);
       const neighbour = data[0].borders[0];
       console.log(neighbour);
 
@@ -93,7 +90,7 @@ const getCountryData = function (country) {
       return fetch(`https://restcountries.eu/rest/v2/name/${neighbour}?fullText=true`);
     })
     .then(response => response.json())
-    .then(data => renderCountry(data, 'neighbour'))
+    .then(data => renderCountry(data[0], 'neighbour'))
 
 };
 
