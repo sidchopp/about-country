a30679ddf0005209cd3b73d9f0b255be07bae168
Sid <sidchopp@gmail.com>
2021-08-17 12:02:08 -0600

    html/css slight modifications

--- script.js
+++ script.js
@@ -49,11 +49,11 @@ const renderCountry = function (data, className = '') {
     <img class="country__img" src="${data.flag}" />
     <div class="country__data">
       <h3 class="country__name">${data.name}</h3>
+      <h3 class="country__capital">${data.capital}</h3>
       <h4 class="country__region">${data.region}</h4>
-      <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} people</p>
+     <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)}</p>
       <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
       <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-      <p class="country__row"><span>Capital:</span>${data.capital}</p>
     </div>
   </article>`;
   countriesContainer.insertAdjacentHTML('beforeend', html);
